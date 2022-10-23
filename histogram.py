e7e40bb8e186ca30a9a5a7db98422389b11b12e0
Eun K Sung <113143969+eunksung@users.noreply.github.com>
2022-10-22 22:50:31 -0700

    Add files via upload

--- histogram.py
+++ histogram.py
@@ -1,7 +1,14 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('us-counties.csv')
+df = pd.read_csv("https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv")
 
-covid_california = df[(df.state == "California")]
-covid_california.plot(x='date', y='cases', style='l')
+histo_plot = df['deaths'].hist(bins=200)
+histo_plot.set_title('Deaths in a day by COVID-19')
+histo_plot.set_xlabel('Deaths Number')
+histo_plot.set_ylabel('Frequency')
+
+plt.xlim([0, 1000])
+plt.ylim([0, 500000])
+plt.show()
