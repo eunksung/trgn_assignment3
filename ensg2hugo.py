1e2c69b12c06c933000a636de101464b6695a5e6
Eun K Sung <113143969+eunksung@users.noreply.github.com>
2022-10-24 02:05:03 -0700

    Add files via upload

--- ensg2hugo.py
+++ ensg2hugo.py
@@ -1,6 +1,7 @@
 import sys
 import fileinput
 import re
+import pandas as pd
 
 dictionary=sys.argv[1]
 Lookup_ensemb={}
@@ -12,7 +13,6 @@ for each_line_of_text in fileinput.input(dictionary):
             Lookup_ensemb[ensgcd[0]] = hugonm[0]
 
 sample = open('expression_analysis_new.csv', 'w')
-print('Unknown Number,Gene ID,Gene Type,LogFC,AveExpr,Gene Stable ID Ver,Gene name,Gene Type,Gene Start,Gene End', file = sample) 
 change_file=sys.argv[2]
 for each_line_of_text in fileinput.input(change_file):
     splitcolumn_array = re.split(',',each_line_of_text.replace('"' ,'').replace('\n',''))
@@ -22,4 +22,18 @@ for each_line_of_text in fileinput.input(change_file):
         resmod = res.replace("\'", "")
         print(resmod, file = sample)
 sample.close()
-        
+
+import csv
+import pandas as pd
+
+with open("expression_analysis_new.csv", "r") as source:
+    reader = csv.reader(source)
+      
+    with open("expression_analysis_final.csv", "w") as result:
+        writer = csv.writer(result)
+        for r in reader:
+            
+            writer.writerow((r[6], r[2]))
+
+df = pd.read_csv ('expression_analysis_final.csv')
+print(df)
